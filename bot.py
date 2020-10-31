c521be5dc662fb45c931b9bc2348f703b6d2ae9e
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-10-31 19:54:27 +0100

    Dog bot

--- bot.py
+++ bot.py
@@ -2,48 +2,53 @@ import os
 import sys
 import time
 import random
-import telepot
+# import telepot
 import datetime
-from gpiozero import LED
+# from gpiozero import LED
 from dotenv import load_dotenv
 from os.path import join, dirname
 
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+# from telegram.ext.dispatcher import run_async
+import requests
+import re
 
-#LED
-def on():
-    my_led.on()
-    print('ON')
-    return
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
 
-def off():
-    my_led.off()
-    print('OFF')
-    return
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
 
+def dog(update, context):
+    url = get_image_url()
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id=chat_id, photo=url, caption="Dog caption")
 
-def handle(msg):
-    chat_id = msg['chat']['id']
-    command = msg['text']
-
-    print('Got command: %s' % command)
-
-    if command == '/on':
-        bot.sendMessage(chat_id, on())
-    elif command =='/off':
-       bot.sendMessage(chat_id, off())
-       
+def help(update, context):
+    #chat_id = update.message.chat_id
+    pass
 
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-my_token = os.environ.get('MY_TOKEN')
-print('token init')
-my_led = LED(17)
-print('LED init')
-bot = telepot.Bot(my_token)
-bot.message_loop(handle)
-print('. . .')
-print('I am listening...')
+def main():
+    print('Start listening . . .')
 
-while 1:
-     time.sleep(10)
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    my_token = os.environ.get('MY_TOKEN')
+
+    updater = Updater(my_token, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('dog',dog))
+    #dp.add_handler(CommandHandler('help',help))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
